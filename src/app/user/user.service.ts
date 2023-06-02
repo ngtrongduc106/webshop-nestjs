843a4fb103274009878dfd10116eb80b48ad446d
Nguyễn Trọng Đức <ngtrongduc106@gmail.com>
2023-06-02 23:15:13 +0700

    [update] user profile

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -4,6 +4,7 @@ import { UsersEntity } from "src/database/entities/Users.entity";
 import { Repository } from "typeorm";
 import { TokenDTO } from "./dtos/Token.dto";
 import { City, District, UserAddress, UserProfileResult } from "src/utils/type";
+import { cities } from "src/common/data/locate/vn/cities";
 
 @Injectable()
 export class UserService {
@@ -11,7 +12,7 @@ export class UserService {
         @InjectRepository(UsersEntity) private userEntity: Repository<UsersEntity>
     ) { }
 
-    async handleProfile(data: TokenDTO) {
+    async handleProfile(data: TokenDTO): Promise<UserProfileResult> {
         const user = await this.userEntity.findOne({
             where: {
                 userId: data.user.userId,
@@ -33,17 +34,15 @@ export class UserService {
         const addressArr: UserAddress[] = []
 
         user.userAddress.map((x) => {
-            const cities: City[] = require(`../../common/data/locate/${x.countryName}/cities`)
-            const districts: District[] = require(`../../common/data/locate/${x.countryName}/districts`)
-
-            console.log(cities)
+            const { cities } = require(`../../common/data/locate/${x.countryName}/cities`)
+            const { districts } = require(`../../common/data/locate/${x.countryName}/districts`)
 
             const address: UserAddress = {
                 countryName: x.countryName,
                 cityId: x.cityId,
-                cityName: cities.find(y => y.code == x.cityId).name,
+                cityName: cities.find((y: City) => y.code == x.cityId).name,
                 districtId: x.districtId,
-                districtName: districts.find(y => y.code == x.districtId).name,
+                districtName: districts.find((y: District) => y.code == x.districtId).name,
                 addressHome: x.addressHome
             }
             addressArr.push(address)
