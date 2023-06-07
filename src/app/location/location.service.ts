c26cf3a754d2f9068adb4dd9b08372191683ec5d
Nguyễn Trọng Đức <ngtrongduc106@gmail.com>
2023-06-07 07:17:56 +0700

    [update] Task location

--- src/app/location/location.service.ts
+++ src/app/location/location.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Inject, Injectable } from "@nestjs/common";
 import { ILocationService } from "./location";
 import { Locate_City, Locate_District, Location_GetAllCities_Details, Location_GetDistrict_Details } from "src/utils/type";
 import { Services } from "src/utils/constants";
@@ -13,11 +13,19 @@ export class LocationService implements ILocationService {
     handleGetAllCities(data: Location_GetAllCities_Details): Locate_City[] {
         const cities: Locate_City[] = this.locateService.getAllCities(data.country);
 
+        if (cities.length <= 0) {
+            throw new HttpException("City not found", HttpStatus.NOT_FOUND);
+        }
+
         return cities;
     }
 
     handleGetDistricts(data: Location_GetDistrict_Details): Locate_District[] {
         const districts: Locate_District[] = this.locateService.getAllDistrictsByCityId(data.country, data.cityId);
+
+        if (districts.length <= 0) {
+            throw new HttpException("City not found", HttpStatus.NOT_FOUND);
+        }
 
         return districts;
     }
